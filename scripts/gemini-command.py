0033b8228b6124aa3962f21e8e21971251a1a39c
Rvn0xsy <rvn0xsy@gmail.com>
2024-07-30 20:29:19 +0800

    add gemini commandline prompt

---
+++ scripts/gemini-command.py
@@ -0,0 +1,43 @@
+
+"""
+Install the Google AI Python SDK
+
+$ pip install google-generativeai
+
+See the getting started guide for more information:
+https://ai.google.dev/gemini-api/docs/get-started/python
+"""
+
+import os
+from rich.console import Console
+from rich.markdown import Markdown
+import google.generativeai as genai
+
+genai.configure(api_key="[TOKEN]")
+
+# Create the model
+# See https://ai.google.dev/api/python/google/generativeai/GenerativeModel
+generation_config = {
+  "temperature": 1,
+  "top_p": 0.95,
+  "top_k": 64,
+  "max_output_tokens": 8192,
+  "response_mime_type": "text/plain",
+}
+
+model = genai.GenerativeModel(
+  model_name="gemini-1.5-pro",
+  generation_config=generation_config,
+  # safety_settings = Adjust safety settings
+  # See https://ai.google.dev/gemini-api/docs/safety-settings
+)
+
+chat_session = model.start_chat(
+  history=[
+  ]
+)
+console = Console()
+message = input(">")
+response = chat_session.send_message(message)
+markdown = Markdown(response.text)
+console.print(markdown)
